a68badf8150e3985ec1bdcfeb3f68a174eb6294c
Mireya Sánchez Pinzón <mireyasanchz02@gmail.com>
2023-04-17 21:22:09 +0200

    Creación de los proyectos de los microservicios

---
+++ Microservicio-Reservas/build.gradle.kts
@@ -0,0 +1,79 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val koin_ksp_version: String by project
+val koin_ktor_version: String by project
+val junit_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.8.10"
+    id("io.ktor.plugin") version "2.2.4"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+    id("com.google.devtools.ksp") version "1.8.0-1.0.8"
+}
+
+group = "es.dam"
+version = "0.0.1"
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    //Tests
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("io.mockk:mockk:1.13.4")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junit_version")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junit_version")
+
+    // Koin - Core
+    implementation("io.insert-koin:koin-core:3.3.3")
+
+    // Koin Anotaciones
+    implementation("io.insert-koin:koin-annotations:1.1.1")
+    implementation("io.ktor:ktor-client-content-negotiation:2.2.4")
+    implementation("io.ktor:ktor-serialization-gson:2.2.4")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    ksp("io.insert-koin:koin-ksp-compiler:1.1.1")
+
+    // Mongo Reactivo
+    implementation("org.litote.kmongo:kmongo-async:4.8.0")
+    implementation("org.litote.kmongo:kmongo-coroutine:4.8.0")
+
+    // Serializar KMongo
+    implementation("org.litote.kmongo:kmongo-id-serialization:4.8.0")
+
+    // Koin
+    implementation("io.insert-koin:koin-ktor:$koin_ktor_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_ktor_version")
+    implementation("io.insert-koin:koin-annotations:$koin_ksp_version")
+}
+
+sourceSets.main {
+    java.srcDirs("build/generated/ksp/main/kotlin")
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ Microservicio-Usuarios/build.gradle.kts
@@ -0,0 +1,55 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.0.5"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.spring") version "1.7.22"
+}
+
+group = "es.dam"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    //Serialización
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+
+    //Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+
+    // Test
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Mockk
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+
+    //Spring security
+    //implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // JWT
+    //implementation("com.auth0:java-jwt:4.2.1")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
