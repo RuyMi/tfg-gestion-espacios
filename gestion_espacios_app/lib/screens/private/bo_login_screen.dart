05f3a808d1ca4d0fc65d61090e40bae1a24dabb8
Alejandro Sánchez Monzón <alejandrosanchezmonzon1@gmai.com>
2023-06-02 13:30:52 +0200

    Cambios menores

--- gestion_espacios_app/lib/screens/private/bo_login_screen.dart
+++ gestion_espacios_app/lib/screens/private/bo_login_screen.dart
@@ -16,139 +16,136 @@ class BOLoginScreen extends StatelessWidget {
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Container(
-          margin: const EdgeInsets.all(20),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/logo.png'),
-                const SizedBox(height: 10),
-                const Text(
-                  'BackOffice',
+      body: Container(
+        margin: const EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/logo.png'),
+              const SizedBox(height: 10),
+              const Text(
+                'BackOffice',
+                style: TextStyle(
+                  fontFamily: 'KoHo',
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 50),
+              SizedBox(
+                width: 400,
+                child: TextField(
+                  onChanged: (value) => username = value,
+                  cursorColor: theme.colorScheme.secondary,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(
+                        color: theme.colorScheme.onSurface,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(
+                        color: theme.colorScheme.onSurface,
+                      ),
+                    ),
+                    labelText: 'Nombre de usuario',
+                    labelStyle: TextStyle(
+                        fontFamily: 'KoHo', color: theme.colorScheme.surface),
+                    prefixIcon: Icon(Icons.person_rounded,
+                        color: theme.colorScheme.surface),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: 400,
+                child: TextField(
+                  onChanged: (value) => password = value,
+                  cursorColor: theme.colorScheme.secondary,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(
+                        color: theme.colorScheme.onSurface,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(
+                        color: theme.colorScheme.onSurface,
+                      ),
+                    ),
+                    labelText: 'Contraseña',
+                    labelStyle: TextStyle(
+                        fontFamily: 'KoHo', color: theme.colorScheme.surface),
+                    prefixIcon: Icon(Icons.lock_rounded,
+                        color: theme.colorScheme.surface),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  authProvider.login(username, password).then(
+                    (usuario) {
+                      final loginSucceed = authProvider.loginSucceed;
+                      final roles = authProvider.usuario.userRole;
+
+                      if (loginSucceed && roles.contains('ADMINISTRATOR')) {
+                        Navigator.pushNamed(context, '/home-bo');
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (context) => const MyErrorMessageDialog(
+                            title: 'Error al iniciar sesión',
+                            description: 'Usuario o contraseña incorrectos.',
+                          ),
+                        );
+                      }
+                    },
+                  ).catchError((error) {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return const MyErrorMessageDialog(
+                            title: 'Error al iniciar sesión',
+                            description: 'Usuario o contraseña incorrectos.',
+                          );
+                        });
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  backgroundColor: theme.colorScheme.secondary,
+                ),
+                child: Text('Validar',
+                    style: TextStyle(
+                        color: theme.colorScheme.onSecondary,
+                        fontFamily: 'KoHo')),
+              ),
+              const SizedBox(height: 10),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login');
+                },
+                style: TextButton.styleFrom(
+                  foregroundColor: theme.colorScheme.onBackground,
+                ),
+                child: Text(
+                  'Acceso al servicio público.',
                   style: TextStyle(
                     fontFamily: 'KoHo',
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
+                    color: theme.colorScheme.secondary,
                   ),
                 ),
-                const SizedBox(height: 50),
-                SizedBox(
-                  width: 400,
-                  child: TextField(
-                    onChanged: (value) => username = value,
-                    cursorColor: theme.colorScheme.secondary,
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(
-                          color: theme.colorScheme.onSurface,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(
-                          color: theme.colorScheme.onSurface,
-                        ),
-                      ),
-                      labelText: 'Nombre de usuario',
-                      labelStyle: TextStyle(
-                          fontFamily: 'KoHo', color: theme.colorScheme.surface),
-                      prefixIcon: Icon(Icons.person_rounded,
-                          color: theme.colorScheme.surface),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  width: 400,
-                  child: TextField(
-                    onChanged: (value) => password = value,
-                    cursorColor: theme.colorScheme.secondary,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(
-                          color: theme.colorScheme.onSurface,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(
-                          color: theme.colorScheme.onSurface,
-                        ),
-                      ),
-                      labelText: 'Contraseña',
-                      labelStyle: TextStyle(
-                          fontFamily: 'KoHo', color: theme.colorScheme.surface),
-                      prefixIcon: Icon(Icons.lock_rounded,
-                          color: theme.colorScheme.surface),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    authProvider.login(username, password).then(
-                      (usuario) {
-                        final loginSucceed = authProvider.loginSucceed;
-                        final roles = authProvider.usuario.userRole;
-
-                        if (loginSucceed && roles.contains('ADMINISTRATOR')) {
-                          Navigator.pushNamed(context, '/home-bo');
-                        } else {
-                          showDialog(
-                            context: context,
-                            builder: (context) => const MyErrorMessageDialog(
-                              title: 'Error al iniciar sesión',
-                              description: 'Usuario o contraseña incorrectos.',
-                            ),
-                          );
-                        }
-                      },
-                    ).catchError((error) {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return const MyErrorMessageDialog(
-                              title: 'Error al iniciar sesión',
-                              description: 'Usuario o contraseña incorrectos.',
-                            );
-                          });
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    backgroundColor: theme.colorScheme.secondary,
-                  ),
-                  child: Text('Validar',
-                      style: TextStyle(
-                          color: theme.colorScheme.onSecondary,
-                          fontFamily: 'KoHo')),
-                ),
-                const SizedBox(height: 10),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/login');
-                  },
-                  style: TextButton.styleFrom(
-                    foregroundColor: theme.colorScheme.onBackground,
-                  ),
-                  child: Text(
-                    'Acceso al servicio público.',
-                    style: TextStyle(
-                      fontFamily: 'KoHo',
-                      color: theme.colorScheme.secondary,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
